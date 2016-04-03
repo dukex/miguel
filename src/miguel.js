e658f25b7370b73515651d69dd642dc46f4dc4cd
Duke <dukekhaos@gmail.com>
2016-04-03 18:18:56 -0300

    refactor

--- src/miguel.js
+++ src/miguel.js
@@ -1,21 +1,11 @@
-class MiguelGame {
-  constructor() {
-    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
-      preload: this.preload.bind(this),
-      create: this.create.bind(this),
-      update: this.update.bind(this)
-    });
+class Player {
+  constructor(game) {
+    this.game = game;
   }
 
-  preload() {
-    this.game.load.atlasXML('seacreatures', 'assets/bgElements_spritesheet.png', 'assets/bgElements_spritesheet.xml');
-    this.game.load.image('road', 'assets/road.png');
-    this.game.load.atlas('miguel', 'assets/miguel_s.png', 'assets/miguel.json');
-  }
-
-  playerMoviment(move) {
+  moviment(move) {
     switch(move) {
-      case 'left':
+      case 'walk':
         return  [13, 15, 6, 9, 4, 25];
       case 'swim':
         return [11,18,20,16,17,10];
@@ -37,70 +27,126 @@ class MiguelGame {
   }
 
   create() {
-    // this.game.stage.backgroundColor = '#4488AA';
+    this._player = this.game.add.sprite(this.game.world.height / 2, 0, 'miguel');
+    this.game.physics.arcade.enable(this._player);
+
+
+    this._player.body.bounce.y = 0;
+    this._player.body.gravity.y = 600;
+    this._player.body.collideWorldBounds = true;
+
+    this._player.animations.add('walk', this.moviment('walk'), 10, true);
+    this._player.animations.add('jump', this.moviment('jump'), 10, true);
+    this._player.animations.add('idle', this.moviment('idle'), 1, true);
+  }
+
+  __jumpping() {
+    return this._player.body.y < 499;
+  }
+
+  __flip(original) {
+    this._player.anchor.setTo(.5, .5)
+    this._player.scale.x = original ? 1 : -1;
+  }
+
+  update() {
+    const player = this._player;
+    const cursors = this.game.input.keyboard.createCursorKeys();
+
+    player.body.velocity.x = 0;
+
+    if(cursors.left.isDown || cursors.right.isDown) {
+      this.__flip(cursors.right.isDown)
+      player.body.velocity.x = cursors.right.isDown ? 150 : -150;
+      !this.__jumpping() && player.animations.play('walk', 15, true);
+    }
+
+    if (cursors.up.isDown && !this.__jumpping()) {
+      player.animations.stop();
+      player.animations.play('jump', 3, false);
+      player.body.velocity.y = -400;
+    }
+
+    if(!(this.__jumpping() || cursors.up.isDown || cursors.right.isDown || cursors.left.isDown)) {
+      player.animations.play('idle');
+    }
+  }
+}
+
+class StartState {
+  constructor(game) {
+    this.player = new Player(game);
+  }
+
+  startGame() {
+    debugger
+  }
+
+  create() {
     this.game.stage.backgroundColor = '#F9F9F9';
+    this.player.create();
+
+    const { height, world: { centerX, centerY} } = this.game;
+
+
+    const text = this.game.add.text(centerX, 0, "Miguel", {
+      font: 'Cinzel Decorative',
+      fontSize: 140,
+      align: 'center'
+    });
+    text.anchor.setTo(0.5);
+    text.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
+    this.game.add.tween(text).to({ y: centerY }, 2400, Phaser.Easing.Elastic.Out, true)
+
+    this.game.add.button(centerX, height-(height * 30 / 100), 'start', this.startGame, this, 0,1,2,3)
+
+    const music = this.game.add.audio('synthcity');
+    music.play('', 5, true);
+  }
+
+  update() {
+    this.player.update();
+  }
+}
+
+
+class BootState {
+  preload() {
+    this.game.load.atlasXML('seacreatures', 'assets/bgElements_spritesheet.png', 'assets/bgElements_spritesheet.xml');
+    this.game.load.image('road', 'assets/road.png');
+    this.game.load.atlas('miguel', 'assets/miguel_s.png', 'assets/miguel.json');
+    this.game.load.audio('synthcity', ['assets/audio/synthcity.mp3', 'assets/audio/synthcity.ogg', 'assets/audio/synthcity.flp']);
+  }
+
+  create() {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.pageAlignHorizontally = true;
+    this.game.scale.pageAlignVertically = true;
+    this.game.state.start('start');
+  }
+}
 
-    this.player = this.game.add.sprite(  this.game.world.height / 2, 0, 'miguel');
-    this.game.physics.arcade.enable(this.player);
+class MiguelGame {
+  constructor() {
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'content');
 
-    this.player.body.bounce.y = 0;
-    this.player.body.gravity.y = 600;
-    this.player.body.collideWorldBounds = true;
+    game.state.add('boot', BootState);
+    game.state.add('start', StartState);
+    game.state.start('boot', true);
+  }
 
-    this.player.animations.add('run__left', this.playerMoviment('left'), 10, true);
-    this.player.animations.add('run__right', this.playerMoviment('left'), 10, true);
-    this.player.animations.add('jump', this.playerMoviment('jump'), 10, true);
-    this.player.animations.add('idle', this.playerMoviment('idle'), 1, true);
-
+  create() {
     this.road = this.game.add.group();
     this.road.enableBody = true;
 
     var road = this.road.create(0, this.game.world.height - 20, 'road');
     road.scale.setTo(this.game.world.width, 1);
     road.body.immovable = true;
-
-    var text = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "Miguel");
-    text.anchor.setTo(0.5);
-
-    text.font = 'Cinzel Decorative';
-    text.fontSize = 160;
-    text.align = 'center';
-    text.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
   }
 
 
   update() {
-    var player = this.player;
-    var cursors = this.game.input.keyboard.createCursorKeys();
-
-    player.body.velocity.x = 0;
-
     this.game.physics.arcade.collide(player, this.road);
-
-    var isNotUpping = player.body.y > 499;
-
-    if (cursors.left.isDown) {
-      if(!player.isFlipped) {
-        player.anchor.setTo(.5, .5)
-        player.scale.x = -1;
-        player.isFlipped = true;
-      }
-      player.body.velocity.x = -150;
-      isNotUpping && player.animations.play('run__left', 15, true);
-    } else if (cursors.right.isDown) {
-      if(player.isFlipped) {
-        player.scale.x = 1;
-        player.isFlipped = false;
-      }
-      player.body.velocity.x = 150;
-      isNotUpping && player.animations.play('run__left', 15, true);
-    } else if (cursors.up.isDown && isNotUpping) {
-      player.animations.stop();
-      player.body.velocity.y = -400;
-      player.animations.play('jump', 3, false);
-    } else if (isNotUpping) {
-      player.animations.play('idle');
-    }
   }
 }
