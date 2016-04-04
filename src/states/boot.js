a3fefae3f3350afbeda8be76df41e6277a54dfef
Duke <dukekhaos@gmail.com>
2016-04-04 11:28:26 -0300

    add first state images

--- src/states/boot.js
+++ src/states/boot.js
@@ -3,6 +3,15 @@ class BootState {
     this.game.load.audio('synthcity', ['assets/audio/synthcity.mp3', 'assets/audio/synthcity.ogg', 'assets/audio/synthcity.flp']);
 
     this.game.load.atlasJSONHash('miguel', 'assets/miguel.png', 'assets/miguel.json');
+
+    this.game.load.image('8_background', 'assets/state/8_background.png');
+    this.game.load.image('7_sky', 'assets/state/7_sky.png');
+    this.game.load.image('6_buildings_shadow', 'assets/state/6_buildings_shadow.png');
+    this.game.load.image('5_buildings_shadow', 'assets/state/5_buildings_shadow.png');
+    this.game.load.image('4_buildings_shadow', 'assets/state/4_buildings_shadow.png');
+    this.game.load.image('3_buildings', 'assets/state/3_buildings.png');
+    this.game.load.image('2_trees', 'assets/state/2_trees.png');
+    this.game.load.image('1_street', 'assets/state/1_street.png');
   }
 
   create() {
